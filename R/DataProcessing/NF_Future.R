1cd3135314978ce86440db1ecc910632fd81cd47
sofiemccomb <shemccomb@gmail.com>
2019-12-17 14:07:36 -0600

    Commit pland natural

---
+++ R/DataProcessing/NF_Future.R
@@ -0,0 +1,17 @@
+#Creating NF Script for the Server only, for higher processing power and memory, and better parallel processing.
+library(doParallel)
+#Create function to perform zonal statistics
+compute_ecozone_stats<-function(polygonlayer, rasterstack){
+  
+  registerDoParallel(cores=19) #initiate multiple core for processing
+  #ptm <- proc.time() #time the process
+  result<-foreach (i = 1:nrow(polygonlayer), .combine=rbind) %dopar% { #number of polygons to iterate through
+    #BUNCH OF STUFF HAPPENS RESULTING IN FINAL DF-FINAL SHOULD BE FIPS< AND THEN RBIND ACROSS STATE
+    #DO FOR EACH STATE< WHICH IS POLYGON LAYER an dYEAR
+    return(final)
+  }
+  return(result)
+  #proc.time() - ptm
+  endCluster()
+  
+} #end function compute_ecozone_stats
